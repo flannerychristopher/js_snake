928db43becad80c7fef1607adde9c61f72788b68
flan the dev <flanthedev@gmail.com>
2017-08-14 22:46:41 +0800

    event listener and interval timer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const contentElement = document.getElementById('content');
 
 const gridObj = {
-  renderGrid: function() {
+  render: function() {
     for (let x =  0; x < 40; x++) {
       for (let y = 0; y < 40; y++) {
         let divElement = document.createElement('div');
@@ -15,39 +15,50 @@ const gridObj = {
 
 const snakeObj = {
   position: [20,20],
-  head: document.getElementById('2,3'),
 
-  renderSnake: function() {
+  render: function() {
     let snakeHead = document.getElementById( this.position.toString() );
     console.log(this.position);
-    snakeHead.classList.add('snake');
+    snakeHead.style.backgroundColor = 'red';
   },
 
-  moveR: function() {
+  move: function(key) {
     let snakeHead = document.getElementById( this.position.toString() );
-    snakeHead.classList.remove('snake');
-    this.position[1]++;
-    this.renderSnake();
+    snakeHead.style.backgroundColor = 'white';
+
+    if (key === 38) { // move up
+      this.position[0]--;
+    } else if (key === 39) { // move right
+      this.position[1]++;
+    } else if (key === 40) { // move down
+      this.position[0]++;
+    } else if (key === 37) { // move left
+      this.position[1]--;
+    }
+
+    this.render();
   }
 
-};
-
-window.onload = () => {
-  gridObj.renderGrid();
-  snakeObj.renderSnake();
 };
 
 document.addEventListener('keydown', function(key) {
   key = key || window.event;
   // console.log(key.keyCode);
-  if (key.keyCode == '38') { // up arrow
+  if (key.keyCode === 38) { // up arrow
     console.log('up');
-  } else if (key.keyCode == '39') { // right arrow
+    snakeObj.move(38);
+  } else if (key.keyCode === 39) { // right arrow
     console.log('right');
-    snakeObj.moveR();
-  } else if (key.keyCode == '40') { // down arrow
+    snakeObj.move(39);
+  } else if (key.keyCode === 40) { // down arrow
     console.log('down');
-  } else if (key.keyCode == '37') {
+    snakeObj.move(40);
+  } else if (key.keyCode === 37) {
     console.log('left');
+    snakeObj.move(37);
   }
 });
+
+gridObj.render();
+snakeObj.render();
+let timer = setInterval(snakeObj.move, 500);
